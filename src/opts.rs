55729fcb1c23d6049fd70fc530daaacda53c8d3a
VictorBulba <bulba@asclet.com>
2021-03-23 19:40:30 +0300

    Remove cache_cap from opts

--- src/opts.rs
+++ src/opts.rs
@@ -5,13 +5,11 @@ pub struct EngineOptions {
     pub delimiter: u8,
     /// Indexing buffer, only used at creation. Number of bytes.
     pub buf_capacity: usize,
-    /// Capacity for lru cache. Number of items.
-    pub cache_cap: usize,
 }
 
 impl Default for EngineOptions {
     #[inline]
     fn default() -> Self {
-        Self { delimiter: b',', buf_capacity: 10 * 1024 * 1024, cache_cap: 1024 }
+        Self { delimiter: b',', buf_capacity: 10 * 1024 * 1024 }
     }
 }
